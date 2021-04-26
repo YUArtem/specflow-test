1aeae6ec0507c3e6912d84d9ccfa8b58eeb412d9
Artem Yurchenko <artem.yurchenko@4experience.co>
2021-04-26 11:15:11 +0200

    Test

--- SpecFlowProject/Features/EatingCucumbers.feature
+++
@@ -1,3 +0,0 @@
-Feature: Eating Cucumbers
-  
-Scenario: Do something
--- SpecFlowProject/RunAllTestsAndGenerateReport.bat
+++ SpecFlowProject/RunAllTestsAndGenerateReport.bat
@@ -1,4 +1,6 @@
 dotnet test
 cd "bin\Debug\net5.0"
-livingdoc test-assembly SpecFlowProject.dll -t TestExecution.json
+livingdoc test-assembly SpecFlowProject.dll -t TestExecution.json --output-type JSON
 LivingDoc.html
+curl -X POST https://studio.cucumber.io/cucumber_project/results -F messages=@"C:/Projects/SpecFlowProject/bin/Debug/net5.0/FeatureData.json" -H "project-access-token: 274261537559446913826084341512826921691327000556075304236" -H "provider: github" -H "repo: YUArtem/specflow-test" -H "branch: main" -H "revision: 61c3504" 
+cmd.exe
